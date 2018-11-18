60c473c93762b8964b0ece265581761b1034325a
Dmitry Kulga <coolgadv@gmail.com>
2018-11-18 19:26:36 +0300

    Added syntax usage samples for object operators.

---
+++ language/operators/object-operators.groovy
@@ -0,0 +1,38 @@
+
+// -------------------------
+// Safe navigation operator:
+// -------------------------
+
+class Person { String name }
+
+Person person = new Person(name: 'some')
+assert person?.name == 'some'
+
+person = null
+assert person?.name == null
+
+// --------------------
+// Direct field access:
+// --------------------
+
+class Location {
+
+    public String address
+
+    String getAddress() {
+        "address: $address"
+    }
+
+}
+
+Location location = new Location(address: 'some')
+assert location.address == 'address: some'
+assert location.@address == 'some'
+
+// ------------------
+// Method references:
+// ------------------
+
+String string = 'some string'
+Closure<String> reference = string.&toUpperCase
+assert reference() == 'SOME STRING'
