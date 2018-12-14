d7033feec8a8257ce9558b8ebc91f47a1552c965
Dmitry Kulga <coolgadv@gmail.com>
2018-12-14 19:33:11 +0300

    Added syntax usage samples for Groovy truth expressions.

---
+++ language/semantics/groovy-truth.groovy
@@ -0,0 +1,65 @@
+// ---------------------
+// Boolean expressions :
+// ---------------------
+
+assert true
+assert ! false
+
+// -------
+// Lists :
+// -------
+
+assert [1, 2, 3]
+assert ! []
+
+// ------
+// Maps :
+// ------
+
+assert ['one' : 1]
+assert ! [:]
+
+// ----------
+// Matchers :
+// ----------
+
+assert ('a' =~ /a/)
+assert ! ('b' =~ /a/)
+
+// ---------
+// Strings :
+// ---------
+
+assert ' '
+assert ! ''
+
+// ---------
+// Numbers :
+// ---------
+
+assert 1
+assert ! 0
+
+// -------------------
+// Object references :
+// -------------------
+
+assert new Object()
+assert ! null
+
+// -----------------------
+// Custom implementation :
+// -----------------------
+
+class CustomType {
+
+    String field
+
+    def asBoolean() {
+        field == 'some'
+    }
+
+}
+
+assert new CustomType(field : 'some')
+assert ! new CustomType(field : 'any')
