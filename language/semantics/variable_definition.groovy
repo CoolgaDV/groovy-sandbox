60afa3a99c63ecd5405045d2160b4f6c7930d553
Dmitry Kulga <coolgadv@gmail.com>
2018-12-22 19:24:33 +0300

    Added syntax usage samples for variable definitions.

---
+++ language/semantics/variable_definition.groovy
@@ -0,0 +1,32 @@
+
+// --------------------
+// multiple assignment:
+// --------------------
+
+def (a, b, c) = [1, 2, 3]
+assert a == 1 && b == 2 && c == 3
+
+def (d, e, f) = [4, 5]
+assert d == 4 && e == 5 && f == null
+
+// ---------------------
+// object destructuring:
+// ---------------------
+
+class Person {
+
+    String name
+    int age
+
+    def getAt(int index) {
+        switch (index) {
+            case 0: return name
+            case 1: return age
+            default: throw new IllegalArgumentException()
+        }
+    }
+}
+
+def person = new Person(name: 'some', age: 42)
+def (name, age) = person
+assert name == 'some' && age == 42
